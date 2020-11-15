b1f593238607c05fc48ff9017712e79617650278
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-11-15 14:19:26 -0300

    Aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import formatDate from "./utils/formatDate";
+import "./App.css";
+const Diff = require("diff");
 
 function App() {
+  const [text, setText] = useState("");
+  const [history, setHistory] = useState([]);
+
+  const handleEdit = () => {
+    const diff = Diff.diffChars(
+      history.length > 0 ? history[0].text : "",
+      text
+    );
+
+    console.log(diff);
+
+    const date = new Date();
+    setHistory([{ date: date.getTime(), diff: diff, text: text }, ...history]);
+  };
+
+  const getDiffCount = (diff) => {
+    return diff.reduce(
+      (prev, current) => {
+        if (current.added) {
+          return { removed: prev.removed, added: prev.added + current.count };
+        }
+
+        if (current.removed) {
+          return { removed: prev.removed + current.count, added: prev.added };
+        }
+
+        return prev;
+      },
+      { added: 0, removed: 0 }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form action="">
+        <textarea
+          name="texto"
+          placeholder="Insira o texto"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="button" onClick={handleEdit}>
+          Modificar
+        </button>
+      </form>
+
+      <h3>Histórico de edições</h3>
+      {history.map((change) => (
+        <div className="change" key={change.date}>
+          <span className="date">{formatDate(change.date)}</span>
+          <div>
+            {change.diff.map((part) => (
+              <span
+                className={
+                  part.added || part.removed
+                    ? part.added
+                      ? "added"
+                      : "removed"
+                    : ""
+                }
+              >
+                {part.value}
+              </span>
+            ))}
+          </div>
+
+          <div className="count">
+            <span className="added">+ {getDiffCount(change.diff).added}</span>
+            <span className="removed">
+              - {getDiffCount(change.diff).removed}
+            </span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
